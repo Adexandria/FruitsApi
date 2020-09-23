06ed5add5146ab1267d046ec16b113dca12c1be4
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2020-09-23 11:53:24 +0100

    Documentation for swagger

--- FruitsApi/Startup.cs
+++ FruitsApi/Startup.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,6 +31,28 @@ namespace FruitsApi
      .AddNewtonsoftJson(options =>
      options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
  );
+            services.AddSwaggerGen(setupAction =>
+            {
+                setupAction.SwaggerDoc("FruitsOpenAPISpecification", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title="Fruits Api",
+                    Version = "1.0",
+                    Description="An Api that returns a detailed property of an image of a fruit",
+                    Contact= new Microsoft.OpenApi.Models.OpenApiContact() 
+                    {
+                        Email ="adeolaaderibigbe09@gmail.com",
+                        Name ="Adeola Aderibigbe",
+                        Url =new Uri("https://twitter.com/addiexandria")
+                    }
+               
+
+                });
+                var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
+                setupAction.IncludeXmlComments(xmlPath);
+            });
+           
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,9 +62,25 @@ namespace FruitsApi
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            else
+            {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("An unexpected fault happened. Try again later.");
+                    });
+                });
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(setupAction =>
+            {
+                setupAction.SwaggerEndpoint("/swagger/FruitsOpenAPISpecification/swagger.json", "My Fruits Api");
+                setupAction.RoutePrefix = string.Empty;
+            });
             app.UseRouting();
-
+           
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
